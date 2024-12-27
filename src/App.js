4e7072e2248bd721e74210bb16e3275521e88afb
Pankaj8839 <pankajrajput8839@gmail.com.>
2024-12-27 23:34:29 +0530

    add div with class

--- src/App.js
+++ src/App.js
@@ -13,98 +13,94 @@ function App() {
 
   const customStyles = {
     overlay: {
-      backgroundColor: 'rgba(0, 0, 0, 0.5)', 
+      backgroundColor: 'rgba(0, 0, 0, 0.5)',
     },
     content: {
-   
+
     },
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if(userDetails.phoneNumber.length !== 10){
+    if (userDetails.phoneNumber.length !== 10) {
       alert("Invalid phone number. Please enter a 10-digit phone number.");
       return
     }
     let selectedDate = new Date(userDetails.date);
     let today = new Date();
-    if(selectedDate > today){
+    if (selectedDate > today) {
       alert("Invalid date of birth. Date of birth cannot be in the future.");
       return
     }
     setModalIsOpen(false);
-    let obj={
+    let obj = {
       name: '', email: '', date: '', phoneNumber: ''
     }
-    setUserDetails({obj})
+    setUserDetails({ obj })
   };
 
   return (
     <div className="App">
       <h1>User Details Modal</h1>
       <button onClick={() => setModalIsOpen(true)}>Open Form</button>
-      
-
-
-<Modal
-  isOpen={modalIsOpen}
-  onRequestClose={() => setModalIsOpen(false)}
-  className="mainmodal"
-  style={customStyles}
-  ariaHideApp={false}
-  shouldCloseOnOverlayClick={true} 
->
-<div className="modal">
-
-<div className="modal-content">
-  <h2>Fill Details</h2>
-  <form onSubmit={handleSubmit} className="form">
-    <p>Username:</p>
-    <input
-      type="text"
-      id="username"
-      name="name"
-      value={userDetails.name}
-      onChange={handleChange}
-      required
-    />
-    <p>Email Address:</p>
-    <input
-      type="email"
-      id="email"
-      name="email"
-      value={userDetails.email}
-      onChange={handleChange}
-      required
-    />
-    <p>Phone Number:</p>
-    <input
-      type="text"
-      id="phone"
-      name="phoneNumber"
-      value={userDetails.phoneNumber}
-      onChange={handleChange}
-      required
-    />
-    <p>Date of Birth:</p>
-    <input
-      type="date"
-      id="dob"
-      name="date"
-      value={userDetails.date}
-      onChange={handleChange}
-      required
-    />
-    <button type="submit" className="submit-button">Submit</button>
-  </form>
-  </div>
-
-</div>
-</Modal>
 
 
 
-      
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={() => setModalIsOpen(false)}
+        className="mainmodal"
+        style={customStyles}
+        ariaHideApp={false}
+        shouldCloseOnOverlayClick={true}
+      >
+        <div className="modal">
+
+          <div className="modal-content">
+            <h2>Fill Details</h2>
+            <form onSubmit={handleSubmit} className="form">
+              <p>Username:</p>
+              <input
+                type="text"
+                id="username"
+                name="name"
+                value={userDetails.name}
+                onChange={handleChange}
+                required
+              />
+              <p>Email Address:</p>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                value={userDetails.email}
+                onChange={handleChange}
+                required
+              />
+              <p>Phone Number:</p>
+              <input
+                type="text"
+                id="phone"
+                name="phoneNumber"
+                value={userDetails.phoneNumber}
+                onChange={handleChange}
+                required
+              />
+              <p>Date of Birth:</p>
+              <input
+                type="date"
+                id="dob"
+                name="date"
+                value={userDetails.date}
+                onChange={handleChange}
+                required
+              />
+              <button type="submit" className="submit-button">Submit</button>
+            </form>
+          </div>
+
+        </div>
+      </Modal>
     </div>
   );
 }
